30c62c48a7fd847b669c2fc48fe5fcc23924ad5d
MariaLuisaGabriel <maria.luisa.gabriel.domingues@gmail.com>
2022-09-15 13:02:29 -0300

    :)

---
+++ prolog/teste.pl
@@ -0,0 +1,11 @@
+homem(eduardo).
+homem(silvio).
+mulher(maria).
+mulher(silvia).
+pais(eduardo,silvio,silvia).
+pais(maria,silvio,silvia).
+irmao_de(X,Y) :-
+    mulher(X),
+    homem(Y),
+    pais(X,M,P),
+    pais(Y,M,P).
