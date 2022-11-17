fbef743bf8bbd178979cf54e2b26e222b856bb87
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2022-11-16 21:40:29 -0300

    trab de tad

---
+++ AED1/TAD/2/letraA/a.c
@@ -0,0 +1,162 @@
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int p=0,pos,j,r,p;
+    struct aluno it;
+    Lista *a;
+    while(1)
+    {
+        puts("MENU DAS LISTAS:");
+        puts("================");
+        puts("0-sair;");
+        puts("1-criar lista;");
+        if(p==1)
+        {
+            puts("2-inserir aluno na lista;");
+            puts("3-remover aluno da lista;");
+            puts("4-buscar item da lista;");
+            puts("5-mostrar estado atual da lista;");
+            puts("6-limpar .");
+        }
+        printf("\nescreva seu comando->");
+        scanf("%d",&j);
+        if(j==0) break;
+        switch(j)
+        {
+            case 1:
+            {
+                a = criar();
+                break;
+            }
+            case 2:
+            {
+                puts("\n");
+                puts("INSERCAO DE ELEMENTOS:");
+                puts("======================");
+                puts("1-inserir no inicio da lista;");
+                puts("2-inserir no final da lista;");
+                puts("3-inserir numa posicao especifica.");
+                printf("\n->");
+                scanf("%d",&p);
+                //captura dos dados do aluno a inserir:
+                printf("\nnome do aluno: ");
+                scanf("%[^\n]s",it.nome);
+                printf("\nnumero de matricula: ");
+                scanf("%d",&it.mat);
+                printf("\nnota do aluno: ");
+                scanf("%d",it.n1);
+                switch(p)
+                {
+                    case 1:
+                    {
+                        r = inserirInicio(a,it);
+                        switch(r)
+                        {
+                            case 0:
+                                printf("\nlista cheia!");break;
+                            case 2:
+                                printf("\nlista inexistente!");p=0;break;
+                        }
+                        break;
+                    }
+                    case 2:
+                    {
+                        r = inserirFim(a,it);
+                        switch(r)
+                        {
+                            case 0:
+                                printf("\nlista cheia!");break;
+                            case 2:
+                                printf("\nlista inexistente!");p=0;break;
+                        }
+                        break;
+                    }
+                    case 3:
+                    {
+                        printf("insira a posicao desejada(posicao minima: 0,posicao maxima: 100, o dado sera alocado n): ");
+                        scanf("%d",&pos);
+                        r = inserirPosicao(a,it,pos);
+                        switch(r)
+                        {
+                            case 0:
+                                printf("\nlista cheia!");break;
+                            case 2:
+                                printf("\nlista inexistente!");p=0;break;
+                            case 8:
+                                printf("\nposicao alem dos limites da lista!");break;
+                        }
+                        break;
+                    }
+                }
+                break;
+            }
+            case 3:
+            {
+                puts("\n");
+                puts("REMOCAO DE ELEMENTOS:");
+                puts("=====================");
+                puts("1-remover do inicio da lista;");
+                puts("2-remover do final da lista;");
+                puts("3-remover de uma posicao especifica.");
+                printf("\n->");
+                scanf("%d",&p);
+                switch(p)
+                {
+                    case 1:
+                    {
+                        r = removerInicio(a);
+                        switch(r)
+                        {
+                            case 0:
+                                printf("\nlista vazia!");break;
+                            case 2:
+                                printf("\nlista inexistente!");p=0;break;
+                        }
+                        break;
+                    }
+                    case 2:
+                    {
+                        r = removerFim(a);
+                        switch(r)
+                        {
+                            case 0:
+                                printf("\nlista vazia!");break;
+                            case 2:
+                                printf("\nlista inexistente!");p=0;break;
+                        }
+                        break;
+                    }
+                    case 3:
+                    {
+                        printf("insira a posicao desejada(posicao minima: 0, posicao maxima: 100, o dado sera alocado n): ");
+                        scanf("%d",&pos);
+                        r = removerFim(a);
+                        switch(r)
+                        {
+                            case 0:
+                                printf("\nlista vazia!");break;
+                            case 2:
+                                printf("\nlista inexistente!");p=0;break;
+                            case 8:
+                                printf("\nposicao alem dos limites da lista!");break;
+                        }
+                        break;
+                    }
+                }
+                break;
+            }
+            case 4:
+            {
+
+            }
+        }
+    }
+    puts("FIM DA EDICAO DA LISTA!!");
+    printf("\ntamanho final da lista: %d\n",tamanho(a));
+    puts("LISTA CRIADA: ");
+    mostrar(a);
+    return 0;
+}
---
+++ AED1/TAD/2/letraA/lista.c
@@ -0,0 +1,181 @@
+#include "lista.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct lista {
+    int total;
+    struct aluno valores[MAX];
+}Lista;
+
+Lista *criar()
+{
+    Lista *A = (Lista *)malloc(MAX*sizeof(Lista));
+    return A;
+}
+
+void limpar(Lista*l)
+{
+    l->total = 0;
+}
+
+int inserirInicio(Lista *l,struct aluno it)
+{
+    if(l==NULL) return 2;
+    if(l->total==MAX) return 0;
+    else if(l->total>0)
+    {
+        for(int i=l->total-1;i>0;i--)
+            l->valores[i] = l->valores[i-1];
+        l->valores[0] = it;
+        l->total++;
+        return 1;
+    }
+    else
+    {
+        l->valores[0] = it; l->total++;
+        return 1;
+    }
+}
+
+int inserirFim(Lista *l,struct aluno it)
+{
+    if(l==NULL) return 2;
+    if(l->total==MAX) return 0;
+    else
+    {
+        l->valores[l->total] = it;
+        l->total++;
+        return 1;
+    }
+}
+
+int inserirPosicao(Lista *l,struct aluno it,int pos)
+{
+    if(l==NULL) return 2;
+    if(pos>MAX||pos<1) return 8;
+    if(l->total==MAX) return 0;
+    if(pos == l->total)
+    {
+        l->valores[pos-1] = it;
+        l->total++;
+        return 1;
+    }
+    else
+    {
+        for(int i=l->total-1;i>pos-1;i--)
+            l->valores[i] = l->valores[i-1];
+        l->valores[pos-1] = it;
+        l->total++;
+        return 1;
+    }
+}
+
+int removerInicio(Lista *l)
+{
+    if(l==NULL) return 2;
+    if(l->total == 0) return 0;
+    for(int i=l->total-1;i>0;i--)
+        l->valores[i-1] = l->valores[i];
+    l->total--;
+    return 1;
+}
+
+int removerFim(Lista *l)
+{
+    if(l==NULL) return 2;
+    if(l->total == 0) return 0;
+    l->total--;
+    return 1;
+}
+
+int removerPosicao(Lista *l,int pos)
+{
+    int h;
+    if(l==NULL) return 2;
+    if(l->total == 0) return 0;
+    if(pos>MAX||pos<1) return 8;
+    if(pos == l->total)
+        h = removerFim(l); return 1;
+    for(int i=l->total-1;i>pos-1;i--)
+        l->valores[i-1] = l->valores[i];
+    l->total--;
+    return 1; 
+}
+
+int removerItem(Lista *l,int chave)
+{
+    int j;
+    if(l==NULL) return 2;
+    if(l->total==0) return 0;
+    for(int i=0;i<l->total;i++)
+    {
+        if(l->valores[i].mat == chave)
+        {
+            j = removerPosicao(l,i+1);
+            switch(j)
+            {
+                case 0:
+                {
+                    printf("\nlista vazia!");
+                    return 0;
+                }
+                case 1:
+                {
+                    break;
+                }
+            }
+        }
+        return 1;
+    }
+}
+
+int buscarItemChave(Lista*l,int chave,struct aluno *retorno)
+{
+    if(l == NULL) return 2;
+    if(l->total == 0) return 0;
+    for(int i=0;i<l->total;i++)
+    {
+        if(l->valores[i].mat == chave)
+        {
+            *retorno = l->valores[i];
+        }
+    }
+    return 1;
+}
+
+int buscarPosicao(Lista *l,int pos,struct aluno *retorno)
+{
+    if(l == NULL) return 2;
+    if(l->total == 0) return 0;
+    *retorno = l->valores[pos-1];
+    return 1;
+}
+
+int listaVazia(Lista *l)
+{
+    if(l->total == 0) return 1;
+    return 0;
+}
+
+int listaCheia(Lista *l)
+{
+    if(l->total == MAX) return 1;
+    return 0;
+}
+
+int tamanho(Lista *l)
+{
+    return l->total;
+}
+
+void mostrar(Lista *l)
+{
+    for(int i=0;i<l->total;i++)
+    {
+        printf("\nposicao %d: ",i+1);
+        printf("\naluno: %s.",l->valores[i].nome);
+        printf("\nmatricula: %d.",l->valores[i].mat);
+        printf("\nnota: %f.",l->valores[i].n1);
+        printf("\n\n");
+    }
+}
