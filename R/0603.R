33d0fe5abc4797d7c70b37f5d708dfeca2d367b9
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2023-03-02 11:22:39 -0300

    primeira aula de POO hoje :)

---
+++ R/0603.R
@@ -0,0 +1,7 @@
+#funçoes massa que achei no THE BOOK OF R
+#funçao seq: cria uma sequencia de numeros, com valor inicial a final, com especificaçao para tamanho dos intervalos numericos
+#ex:
+x <- seq(3,39,3)
+#sequencia em x que vai de 3 a 39, de 3 em 3
+
+#
